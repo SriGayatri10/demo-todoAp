7d4ac169c19a8d4598d3a80588e4ba8f65a98cf3
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-23 14:48:03 +0000

    Calling input function.

--- src/components/textBox.js
+++ src/components/textBox.js
@@ -1,6 +1,15 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-console */
 import { React } from 'react';
+import context from '../core/context';
 
-const Textbox = () =>
-	<input type="text"/>;
+const Input = () =>
+	<input
+		type="text"
+		onChange={ (evt) =>
+		// console.log(evt);
 
-export default Textbox;
+			 context.actions.input(evt.target.value) }
+	/>;
+
+export default Input;
