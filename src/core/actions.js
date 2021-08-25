786a65bba41200c2e73f7fa7f556c70a11531dff
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-25 11:46:19 +0000

    Included checkbox.

--- src/core/actions.js
+++ src/core/actions.js
@@ -1,13 +1,24 @@
+/* eslint-disable eqeqeq */
+import config from './config';
+import { rndString } from '@laufire/utils/random';
+
 const input = ({ data }) => ({
 	input: data,
 });
 
+const checkbox = ({ data }) => ({
+	isCompleted: data != 0,
+});
+
 const todo = ({ state }) => ({
-	todo: state.todo.concat({ id: Date.now(), text: state.input }),
+	todo: state.todo
+		.concat({ id: rndString(config.refreshIDLength), text: state.input,
+			isCompleted: state.isCompleted }),
 });
 const actions = {
 	input,
 	todo,
+	checkbox,
 
 };
 
