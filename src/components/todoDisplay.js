6641e5a4f4326f737253da23c76516bfbd86ff31
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-25 11:45:50 +0000

    Onclick is added.

--- src/components/todoDisplay.js
+++ src/components/todoDisplay.js
@@ -1,5 +1,14 @@
+/* eslint-disable react/void-dom-elements-no-children */
 import { React } from 'react';
+import context from '../core/context';
 
-const todoDisplay = (data) => <div key={ data.id }>{data.text}</div>;
+const todoDisplay = (data) => <div>
+	<input
+		type="checkbox"
+		onClick={ () =>
+			context.actions.checkbox(1) }
+	/>
+	<span key={ data.id }>{data.text}</span>
+</div>;
 
 export default todoDisplay;
