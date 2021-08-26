2ba2fe290be8d286bc9d969de543e185adeaf787
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-26 13:53:35 +0000

    Data is created.

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const todos = [{
+
+	id: 'MPROUMAK',
+	text: 'hi',
+	isCompleted: false,
+},
+{
+	id: 'BQUURHNY',
+	text: 'hello',
+	isCompleted: true,
+},
+{
+	id: 'WXFHFGIC',
+	text: 'bye',
+	isCompleted: true,
+},
+{
+	id: 'KFAQWSSB',
+	text: 'welcome',
+	isCompleted: false,
+}];
+
+export default todos;
