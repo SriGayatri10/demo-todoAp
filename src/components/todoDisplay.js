977cd0b82d0087113185e3c1e3760614e460b9e9
SriGayatri10 <srigayatrimn@gmail.com>
2021-09-02 12:59:14 +0000

    A button for removing todo is added.

--- src/components/todoDisplay.js
+++ src/components/todoDisplay.js
@@ -1,14 +1,16 @@
 import { React } from 'react';
 import context from '../core/context';
 
-const todoDisplay = (todo) =>
-	<div key={ todo.id }>
-		<input
-			type="checkbox"
-			checked={ todo.isCompleted }
-			onChange={ () => context.actions.toggleTodo(todo) }
-		/>
-		<span>{todo.text} </span>
-	</div>;
+const todoDisplay = (todo) => <div key={ todo.id }>
+	<input
+		type="checkbox"
+		checked={ todo.isCompleted }
+		onChange={ () => context.actions.toggleTodo(todo) }
+	/>
+	<span>{todo.text} </span>
+	<span>
+		<button onClick={ () => context.actions.removeTodo(todo) }>X</button>
+	</span>
+</div>;
 
 export default todoDisplay;
