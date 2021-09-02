88b952786e150890b1e030284dc7cb6b235bd618
SriGayatri10 <srigayatrimn@gmail.com>
2021-09-02 13:01:48 +0000

    ToggleAll and removetodo is added.

--- src/core/actions.js
+++ src/core/actions.js
@@ -13,11 +13,20 @@ const toggleTodo = ({ state, data }) => ({
 	todos: TodoManager.toggleTodo(state.todos, data),
 });
 
+const toggleAllTodos = ({ state, data }) => ({
+	todos: TodoManager.toggleAllTodos(state.todos, data),
+});
+
+const removeTodo = ({ state, data }) => ({
+	todos: TodoManager.removeTodo(state.todos, data),
+});
+
 const actions = {
 	setInput,
 	addTodo,
 	toggleTodo,
-
+	toggleAllTodos,
+	removeTodo,
 };
 
 export default actions;
