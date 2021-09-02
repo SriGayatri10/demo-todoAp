2e7e3a541821c97c415fadabe6d1217e4e032669
SriGayatri10 <srigayatrimn@gmail.com>
2021-09-02 12:57:22 +0000

    ToggleAll is added.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import context from './core/context';
 import AddButton from './components/addButton';
 import todoDisplay from './components/todoDisplay';
 import textBox from './components/textBox';
+import toggleAll from './components/toggleAll';
 
 const App = () => {
 	console.log(context.state);
 	console.log(context.state.todos);
 	return <span className="text-box">
+		<span>{toggleAll()}</span>
 		<span>{textBox()}</span>
 		<span>{AddButton()}</span>
 		<div> input: { context.state.input } </div>
