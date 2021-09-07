c45734c1f601829519770b6e04d4b4aec5430c66
SriGayatri10 <srigayatrimn@gmail.com>
2021-09-07 13:50:04 +0000

    FilterButton is created.

---
+++ src/components/filterButton.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import { React } from 'react';
+import context from '../core/context';
+import TodoManager from '../services/todoManager';
+
+const filterButton = (filter) => {
+	console.log(filter);
+	const isTodos = TodoManager.getTodosCount(context.state.todos) === 0;
+
+	return isTodos
+		? null
+		: <span>
+			<button
+				onClick={ () => context.actions.setFilter(filter) }
+			>
+				{filter}</button>
+		</span>;
+};
+
+export default filterButton;
