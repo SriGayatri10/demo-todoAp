31b1267ba9b68d685195427e9a825a9e0c715690
SriGayatri10 <srigayatrimn@gmail.com>
2021-09-15 14:11:17 +0000

    Introduced the feature of  Enter&Escape  key.

--- src/components/textBox.js
+++ src/components/textBox.js
@@ -3,14 +3,25 @@
 import { React } from 'react';
 import context from '../core/context';
 
+const getEnterKeyAction = () =>
+	(context.state.editing ? 'editTodo' : 'addTodo');
+
+const actionKeys = {
+	Enter: () => context.actions[getEnterKeyAction()](),
+	Escape: () => context.actions.setInput(''),
+};
+
 const textBox = () =>
 	<input
 		type="text"
 		 value={ context.state.input }
 		onChange={ (evt) => {
-			 console.log(evt);
-
-			 context.actions.setInput(evt.target.value);
+			//  console.log(evt);
+			context.actions.setInput(evt.target.value);
+		} }
+		onKeyUp={ (evt) => {
+			// console.log(evt.code);
+			actionKeys[evt.code] && actionKeys[evt.code]();
 		} }
 	/>;
 
