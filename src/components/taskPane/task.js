92acdb1a5d69e61b803daa770b153f0348c15f64
SriGayatri10 <srigayatrimn@gmail.com>
2021-10-04 13:49:59 +0000

    Add button is created.

--- src/components/taskPane/task.js
+++ src/components/taskPane/task.js
@@ -1,9 +1,13 @@
 import { React } from 'react';
 
+const addButton = () =>
+	<button> + </button>;
+
 const Task = (task) => {
 	const { id, text } = task;
 
 	return <div key={ id }>
+		<span>{addButton()}</span>
 		<span>{text}</span></div>;
 };
 
