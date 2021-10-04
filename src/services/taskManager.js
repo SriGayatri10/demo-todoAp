08053c294a235e3e30a2b509e26b1951d1c392bc
SriGayatri10 <srigayatrimn@gmail.com>
2021-10-04 13:21:44 +0000

    New file TaskManager is created.

---
+++ src/services/taskManager.js
@@ -0,0 +1,22 @@
+import context from '../core/context';
+import { rndString } from '@laufire/utils/random';
+
+const getTask = (text) => ({
+	id: rndString(context.config.idLength),
+	text: text,
+});
+
+const init = () => {
+	context.actions.addTask('Task1');
+	context.actions.addTask('Task2');
+	context.actions.addTask('Task3');
+};
+
+const addTask = (tasks, task) => tasks.concat(getTask(task));
+
+const TaskManager = {
+	init,
+	addTask,
+};
+
+export default TaskManager;
