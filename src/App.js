74b86f71360935274d6366113c7c8ddedda9d5e5
SriGayatri10 <srigayatrimn@gmail.com>
2021-10-04 12:56:55 +0000

    Introduced Todo,TaskPane&useEffect for init func.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-console */
-import { React } from 'react';
+import { React, useEffect } from 'react';
 import './App.scss';
 import context from './core/context';
-import textBox from './components/textBox';
-import toggleAll from './components/toggleAll';
-import todoList from './components/todoList';
-import filterBar from './components/filterBar';
-import clearCompleted from './components/clearCompleted';
-import actionButton from './components/actionButton';
+import TaskManager from './services/taskManager';
+import TaskPane from './components/taskPane';
+import TodoPane from './components';
 
 const App = () => {
+	useEffect(TaskManager.init, []);
 	console.log(context.state);
 	console.log(context.state.todos);
-	return <span className="text-box">
-		<span>{toggleAll()}</span>
-		<span>{textBox()}</span>
-		<span>{actionButton()}</span>
-		<div> input: { context.state.input } </div>
-		<div> Todos: {todoList()}</div>
-		<div> {filterBar()}</div>
-		<div> {clearCompleted()}</div>
-	</span>;
+	  return <div>
+		<span>{ TodoPane()}</span>
+		<span>{TaskPane()}</span>
+	</div>;
 };
 
 export default App;
