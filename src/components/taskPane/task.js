9d103cd59f8f584c88c37806ddd06999e2834fe7
SriGayatri10 <srigayatrimn@gmail.com>
2021-10-04 14:37:27 +0000

    Onclick for addButton is added.

--- src/components/taskPane/task.js
+++ src/components/taskPane/task.js
@@ -1,13 +1,18 @@
 import { React } from 'react';
+import context from '../../core/context';
 
-const addButton = () =>
-	<button> + </button>;
+const addButton = (task) =>
+	<button
+		onClick={ () => {
+			context.actions.addTaskTodo(task);
+		} }
+	> + </button>;
 
 const Task = (task) => {
 	const { id, text } = task;
 
 	return <div key={ id }>
-		<span>{addButton()}</span>
+		<span>{addButton(task)}</span>
 		<span>{text}</span></div>;
 };
 
