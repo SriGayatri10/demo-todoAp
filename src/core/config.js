72c2cdde07f5799502b65e0e5413a66cd9aa97b9
SriGayatri10 <srigayatrimn@gmail.com>
2021-10-05 12:25:43 +0000

    Introduced the key minimumTaskCount & tickerDelay.

--- src/core/config.js
+++ src/core/config.js
@@ -1,8 +1,12 @@
+const milliseconds = 1000;
+const seconds = 5;
 const config = {
 	countStart: 0,
 	increment: 1,
 	refreshIDLength: 8,
 	idLength: 16,
+	minimumTaskCount: 3,
+	tickerDelay: milliseconds * seconds,
 };
 
 export default config;
