1d047a277ab2473ceb2125b2dfc958fabefaadd4
SriGayatri10 <srigayatrimn@gmail.com>
2021-12-10 20:00:27 +0530

    Refactored todoManager test.

--- src/services/todoManager.test.js
+++ src/services/todoManager.test.js
@@ -1,123 +1,120 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable max-lines-per-function */
 /* eslint-disable no-magic-numbers */
 import TodoManager from './todoManager';
+import { rndString } from '@laufire/utils/random';
 // eslint-disable-next-line max-statements
 describe('todoManager', () => {
 	const { addTodo, toggleTodo, toggleAllTodos, getActiveChecked,
 		getActiveTodos, removeTodo, getClearCompletedCount, clearCompleted,
 		getTodosCount, setFilter, editTodo } = TodoManager;
+	const existingTodos = [
+		{ id: rndString(16), text: 'hi', isCompleted: false },
+		{ id: rndString(16), text: 'hello', isCompleted: false },
+	];
+	const [impactedTodo, unImpactedTodo] = existingTodos;
 
 	test('Add Todo - adds the given todo', () => {
-		const result = addTodo([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		], 'welcome');
+		const result = addTodo(existingTodos, 'welcome');
 
-		expect(result).toEqual([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-			{ id: expect.any(String), text: 'welcome', isCompleted: false },
-		]);
+		expect(result).toEqual([...existingTodos,
+			{ id: expect.any(String), text: 'welcome', isCompleted: false }]);
 	});
 	test('Toggle Todo', () => {
-		const result = toggleTodo([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		], { id: 'MFMULLYR', text: 'hi', isCompleted: false },);
+		const expectation = [
+			{ ...impactedTodo, isCompleted: !impactedTodo.isCompleted },
+			unImpactedTodo,
+		];
 
-		expect(result).toEqual([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: true },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		]);
+		const result = toggleTodo(existingTodos, impactedTodo);
+
+		expect(result).toEqual(expectation);
 	});
 	test('ToggleAllTodos - Updating  isCompleted', () => {
-		const result = toggleAllTodos([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		], true);
+		const isChecked = true;
+		const results = toggleAllTodos(existingTodos, isChecked);
 
-		expect(result).toEqual([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: true },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: true },
-		]);
+		results.map((result, index) => {
+			expect(result).toEqual({
+				 ...existingTodos[index], isCompleted: isChecked,
+			});
+		});
 	});
 	test('getActiveChecked - Getting No of activeTodos present', () => {
-		const noOfActiveTodos = getActiveChecked([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		]);
+		const noOfActiveTodos = getActiveChecked(existingTodos);
 
 		expect(noOfActiveTodos).toEqual(2);
 	});
 	test('Obtaining length of activeTodos', () => {
-		const length = getActiveTodos([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		]);
+		const length = getActiveTodos(existingTodos);
 
 		expect(length).toEqual(2);
 	});
 
 	test('Remove Todo - removes the selected todo', () => {
-		const afterTodoRemoval = removeTodo([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		], { id: 'BWEGNGHV', text: 'hello', isCompleted: false });
+		const afterTodoRemoval = removeTodo(existingTodos, impactedTodo);
 
-		expect(afterTodoRemoval).toEqual([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-		]);
+		expect(afterTodoRemoval).toEqual([unImpactedTodo]);
 	});
 	test('clearCompletedCount - gets the count of clearCompleted', () => {
-		const length = getClearCompletedCount([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: true },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-			{ id: 'RAEINOGV', text: 'welcome', isCompleted: false },
-		]);
+		const length = getClearCompletedCount(existingTodos);
 
-		expect(length).toEqual(1);
+		expect(length).toEqual(0);
 	});
 	test('Clear Completed', () => {
-		const clearcompleted = clearCompleted([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: true },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-			{ id: 'RAEINOGV', text: 'welcome', isCompleted: false },
-		]);
+		const clearcompleted = clearCompleted(existingTodos);
 
-		expect(clearcompleted).toEqual([
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-			{ id: 'RAEINOGV', text: 'welcome', isCompleted: false },
-		]);
+		expect(clearcompleted).toEqual(existingTodos);
 	});
 	test('TodosCount- Count the no of todos', () => {
-		const length = getTodosCount([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		]);
+		const length = getTodosCount(existingTodos);
 
 		expect(length).toEqual(2);
 	});
 	test('setFilter - set a particular filter which is required', () => {
-		const result = setFilter([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: true },
-		], 'Active');
+		const activeTodos = [{
+			id: rndString(16),
+			text: 'hi',
+			isCompleted: false,
+		}];
+		const completedTodos = [{
+			id: rndString(16),
+			text: 'hello',
+			isCompleted: true,
+		}];
+		const inputTodos = [...activeTodos, ...completedTodos];
+		const expectations = [
+			{
+				filter: 'All',
+				todos: inputTodos,
+			 },
+			{
+				filter: 'Active',
+				todos: activeTodos,
+			 },
+			{
+				filter: 'Completed',
+			 	todos: completedTodos,
+			},
+		];
 
-		expect(result).toEqual([
-			{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-		]);
+		expectations.map(({ filter, todos }) => {
+			const result = setFilter(inputTodos, filter);
+
+			expect(result).toEqual(todos);
+		});
 	});
 	test('EditTodo -  edit the existing todos present', () => {
+		const data = 'bye';
 		const result = editTodo(
-			[
-				{ id: 'MFMULLYR', text: 'hi', isCompleted: false },
-				{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-			], { id: 'MFMULLYR', text: 'welcome', isCompleted: false }, 'bye'
+			existingTodos, impactedTodo, data
 		);
+		const expectation = [
+			{ ...impactedTodo, text: data },
+			unImpactedTodo,
+		];
 
-		expect(result).toEqual([
-			{ id: 'MFMULLYR', text: 'bye', isCompleted: false },
-			{ id: 'BWEGNGHV', text: 'hello', isCompleted: false },
-		]);
+		expect(result).toEqual(expectation);
 	});
 });
