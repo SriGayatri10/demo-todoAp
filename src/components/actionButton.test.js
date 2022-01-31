1161983efca9ce17aec5f28eaa2a4446e8f1268b
SriGayatri10 <srigayatrimn@gmail.com>
2022-01-31 18:48:53 +0530

    Completed actionButton test.

---
+++ src/components/actionButton.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable max-lines-per-function */
+
+import { render } from '@testing-library/react';
+import actionButton from './actionButton';
+import * as editButton from './editButton';
+import * as AddButton from './addButton';
+
+describe('ActionButton', () => {
+	test('EditButton', () => {
+		const context = {
+			state: {
+				editing: true,
+			},
+		};
+
+		jest.spyOn(editButton, 'default')
+			.mockReturnValue(<div role="editButton"/>);
+		const { getByRole } = render(actionButton(context));
+
+		expect(getByRole('editButton')).toBeInTheDocument();
+		expect(editButton.default).toHaveBeenCalledWith();
+	});
+	test('AddButton', () => {
+		const context = {
+			state: {
+				editing: false,
+			},
+		};
+
+		jest.spyOn(AddButton, 'default')
+			.mockReturnValue(<div role="AddButton"/>);
+		const { getByRole } = render(actionButton(context));
+
+		expect(getByRole('AddButton')).toBeInTheDocument();
+		expect(AddButton.default).toHaveBeenCalledWith();
+	});
+});
