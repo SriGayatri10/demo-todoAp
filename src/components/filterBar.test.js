6823db0ba1e92d7b3cfdda2024301c8c6cbba15c
SriGayatri10 <srigayatrimn@gmail.com>
2022-02-17 14:56:28 +0530

    Completed test filterBar.

---
+++ src/components/filterBar.test.js
@@ -0,0 +1,23 @@
+import { React } from 'react';
+import * as filterButton from './filterButton';
+import { render } from '@testing-library/react';
+import filterBar from './filterBar';
+
+test('FilterBar', () => {
+	const context = {
+		actions: {},
+		state: {},
+	};
+	const filters = ['All', 'Active', 'Completed'];
+
+	jest.spyOn(filterButton, 'default')
+		.mockReturnValue(<div role="filterButton"/>);
+
+	const { getAllByRole } = render(filterBar(context));
+
+	filters.map((filter, index) => {
+		expect(filterButton.default)
+			.toHaveBeenCalledWith({ ...context, data: filter });
+		expect(getAllByRole('filterButton')[index]).toBeInTheDocument();
+	});
+});
