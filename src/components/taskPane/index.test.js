19eb5c51045a6968ffebc281a06b1e079857e0e1
SriGayatri10 <srigayatrimn@gmail.com>
2022-02-18 19:56:15 +0530

    Completed test for index.

---
+++ src/components/taskPane/index.test.js
@@ -0,0 +1,21 @@
+import { React } from 'react';
+import { render } from '@testing-library/react';
+import TaskPane from './index';
+import * as TaskList from './taskList';
+test('TaskPane', () => {
+	const context = {
+		actions: {},
+		state: {
+			tasks: [],
+		},
+	};
+
+	jest.spyOn(TaskList, 'default')
+		.mockReturnValue(<div role="TaskList"/>);
+	const { getByRole } = render(TaskPane(context));
+
+	expect(getByRole('TaskPane')).toBeInTheDocument();
+	expect(getByRole('TaskPane')).toHaveTextContent('Task');
+	expect(getByRole('TaskList')).toBeInTheDocument();
+	expect(TaskList.default).toHaveBeenCalledWith(context);
+});
