845062c4181ca1a758057b82915788b5c03661a1
SriGayatri10 <srigayatrimn@gmail.com>
2022-02-18 19:57:32 +0530

    Completed test for task.

---
+++ src/components/taskPane/task.test.js
@@ -0,0 +1,47 @@
+import { fireEvent, render } from '@testing-library/react';
+import Task from './task';
+
+describe('Task', () => {
+	const context = {
+		actions: {
+			addTaskTodo: jest.fn(),
+			removeTask: jest.fn(),
+		},
+	};
+	const task = {
+		id: '',
+		text: 'Program circuit',
+	};
+
+	test('Displays the AddButton,text & RemoveButton', () => {
+		const component = render(Task({ ...context, data: task }))
+			.getByRole('Task');
+
+		expect(component).toBeInTheDocument();
+		expect(component).toHaveTextContent('Program circuit');
+	});
+
+	test('OnClicking the AddButton', () => {
+		const component = render(Task({ ...context, data: task }))
+			.getByRole('addButton');
+
+		fireEvent.click(component);
+
+		expect(context.actions.addTaskTodo)
+			.toHaveBeenCalledWith(task);
+		expect(context.actions.removeTask)
+			.toHaveBeenCalledWith(task);
+		expect(component).toHaveTextContent('+');
+	});
+
+	test('OnClicking the RemoveButton', () => {
+		const component = render(Task({ ...context, data: task }))
+			.getByRole('removeButton');
+
+		fireEvent.click(component);
+
+		expect(context.actions.removeTask)
+			.toHaveBeenCalledWith(task);
+		expect(component).toHaveTextContent('x');
+	});
+});
